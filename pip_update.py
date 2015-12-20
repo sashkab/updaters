d252d9215b33fb6607c15bffc0e4829c3436e473
Aleks Bunin <github@compuix.com>
2015-12-19 21:48:27 -0500

    Fixed couple bugs introduced earlier.
    - Require pip and use version from pip._vendor.packaging;
    - Simplify, rename variable, make less readable and other crap I did when I had nothing else to do.

--- pip_update.py
+++ pip_update.py
@@ -12,11 +12,10 @@ import json
 from subprocess import call
 import argparse
 
-try:
-    from packaging.version import parse
-except ImportError:
-    from pip._vendor.packaging.version import parse
+from pip._vendor.packaging.version import parse
 
+PYPI_URL = 'https://pypi.python.org/pypi'
+VERSION = '2.0'
 
 def notification(title='', subtitle='', message=''):
     """ Uses terminal-notifier for showing notifications."""
@@ -24,40 +23,44 @@ def notification(title='', subtitle='', message=''):
         call(['terminal-notifier', '-title', title, '-subtitle', subtitle, '-message', message], stdin=null)
 
 
-def get_version(package, url_pattern='https://pypi.python.org/pypi/{package}/json'):
+def get_version(package, url_pattern=PYPI_URL + '/{package}/json'):
     """Return version of package on pypi.python.org using json."""
-    r = requests.get(url_pattern.format(package=package))
-    v = parse('0.0')
-    if r.status_code == requests.codes.ok:
-        j = json.loads(r.text.encode(r.encoding))
+    req = requests.get(url_pattern.format(package=package))
+    v = parse('0')
+    if req.status_code == requests.codes.ok:
+        j = json.loads(req.text.encode(req.encoding))
         if 'releases' in j:
-            rls = j['releases']
-            for x in rls:
-                v = max(v, parse(x))
+            releases = j['releases']
+            for release in releases:
+                ver = parse(release)
+                if not ver.is_prerelease:
+                    v = max(v, ver)
     return v
 
 
 def main():
     """Main function"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--stdout', action='store_true', help="Don't use notification, output to stdout")
-    parser.add_argument('--markdown', action='store_true', help="Enable markdown output")
+    parser = argparse.ArgumentParser(version=VERSION)
+    parser.add_argument('-S', '--stdout', action='store_true', help="Don't use notification, output to stdout")
+    parser.add_argument('-M', '--markdown', action='store_true', help="Enable markdown output")
     args = parser.parse_args()
-    new = []
+
+    updates = []
     for dist in pip.get_installed_distributions():
-        version = get_version(dist)  # TODO: Capitalize first letter (?)
-        if not version.is_prerelease and version > parse(dist.version):
+        pypi_version = get_version(dist.project_name)
+        if not pypi_version.is_prerelease and pypi_version > dist.parsed_version:
+            new_version = str(pypi_version)
+            url = '{pypi}/{dist.project_name}/{ver}'.format(pypi=PYPI_URL, dist=dist, ver=new_version)
             if args.markdown:
-                url = '{pypi}/{dist.project_name}/{ver}'.format(pypi=PYPI_URL, dist=dist, ver=available[0])
-                new.append('{dist.project_name} {dist.version} -> [{available}]({url})'.format(dist=dist,
-                                                    available=str(version), url=url))
+                updates.append('{dist.project_name} {dist.version} -> [{n}]({u})'.format(dist=dist, n=new_version, u=url))
             else:
-                new.append('{dist.project_name} {dist.version} -> {available}'.format(dist=dist, available=str(version)))
-    if new:
-        if args.stdout:
-            print('\n'.join(new))
+                updates.append('{dist.project_name} {dist.version} -> {n}'.format(dist=dist, n=new_version))
+
+    if updates:
+        if args.markdown or args.stdout:
+            print('\n'.join(updates))
         else:
-            notification(title='pip updates', message='\n'.join(new))
+            notification(title='pip updates', message='\n'.join(updates))
 
 
 if __name__ == '__main__':
