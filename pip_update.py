8ace11d83e4d9d36c726a4786097307d928d9713
Aleks Bunin <github@compuix.com>
2016-10-03 21:38:07 -0400

    Updated pip_update to add actions: - close - install updates.

--- pip_update.py
+++ pip_update.py
@@ -3,26 +3,30 @@
 # To be called from crontab:
 # 5 7,15,21 * * * $HOME/.virtualenvs/<your venv>/bin/python $HOME/bin/pip_update.py
 #
-# Uses terminal-notifier in OS X Mountain Lion and above. To install it use command:
-# sudo gem install terminal-notifier
+# Uses terminal-notifier 1.7 or above on macOS Sierra. 
+# To install terminal-notifer, use Homebrew: brew install terminal-notifier
 
 from __future__ import print_function
 import pip
 import requests
 import json
-from subprocess import call
+from subprocess import check_output
 import argparse
 from time import sleep
 
 from pip._vendor.packaging.version import parse
 
 PYPI_URL = 'https://pypi.python.org/pypi'
-VERSION = '2.0'
+VERSION = '2.1'
 
 def notification(title='', subtitle='', message=''):
     """ Uses terminal-notifier for showing notifications."""
     with open('/dev/null') as null:
-        call(['terminal-notifier', '-title', title, '-subtitle', subtitle, '-message', message], stdin=null)
+        r = check_output(['terminal-notifier', '-title', title, 
+                          '-subtitle', subtitle, '-message', message,
+                          '-dropdownLabel', 'Close', '-actions', 'Close,Install Updates',
+                        ], stdin=null)
+        return r.strip()
 
 
 def get_version(package, url_pattern=PYPI_URL + '/{package}/json'):
@@ -47,23 +51,25 @@ def main():
     parser.add_argument('-M', '--markdown', action='store_true', help="Enable markdown output")
     args = parser.parse_args()
 
-    updates = []
+    updates = {}
     for dist in pip.get_installed_distributions():
         pypi_version = get_version(dist.project_name)
         if not pypi_version.is_prerelease and pypi_version > dist.parsed_version:
             new_version = str(pypi_version)
             url = '{pypi}/{dist.project_name}/{ver}'.format(pypi=PYPI_URL, dist=dist, ver=new_version)
             if args.markdown:
-                updates.append('{dist.project_name} {dist.version} -> [{n}]({u})'.format(dist=dist, n=new_version, u=url))
+                updates[dist.project_name]  = '{dist.project_name} {dist.version} -> [{n}]({u})'.format(dist=dist, n=new_version, u=url)
             else:
-                updates.append('{dist.project_name} {dist.version} -> {n}'.format(dist=dist, n=new_version))
+                updates[dist.project_name] = '{dist.project_name} {dist.version} -> {n}'.format(dist=dist, n=new_version)
         sleep(1)
 
     if updates:
         if args.markdown or args.stdout:
-            print('\n'.join(updates))
+            print('\n'.join(updates.values()))
         else:
-            notification(title='pip updates', message='\n'.join(updates))
+            action = notification(title='pip updates', message='\n'.join(updates.values()))
+            if action.startswith("Install Updates"):
+                check_output(['pip', 'install', '-U'] + updates.keys())
 
 
 if __name__ == '__main__':
