ca710ab16fc1c879b8c09c30f668a26f516fda76
Aleks Bunin <github@compuix.com>
2021-01-19 21:26:51 -0500

    Support M1

--- homebrew_update.sh
+++ homebrew_update.sh
@@ -4,7 +4,8 @@
 # Uses terminal-notifier in OS X Mountain Lion and above. To install it use command:
 # brew install terminal-notifier
 
-BREW_EXEC='/usr/local/bin/brew'
+BREW_EXEC="$(brew --prefix)/bin/brew"
+TN="$(brew --prefix terminal-notifier)/bin/terminal-notifier"
 
 # https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/Analytics.md
 export HOMEBREW_NO_ANALYTICS=1
@@ -14,7 +15,7 @@ if [ -z "$1" ]; then
     outdated=$($BREW_EXEC outdated)
 
     if [ -n "$outdated" ]; then
-	/usr/local/bin/terminal-notifier -message "$outdated" -title "Homebrew Updates" # -execute $0' upgrade'
+	$TN -message "$outdated" -title "Homebrew Updates" # -execute $0' upgrade'
     fi
 #else
 #    $BREW_EXEC upgrade 2>&1 > /dev/null
